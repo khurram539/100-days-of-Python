f7cd9ceeb8267b48bd86141bae4989a3e5b37fe4
khurram539 <108364751+khurram539@users.noreply.github.com>
2023-03-07 16:13:30 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+age = 20
+price = 19.95
+first_name = "Khurram"
+is_online = False
+print (age)
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+birth_year = input("Enter your birth year: ")
+age = 2023 - int(birth_year)
+print(age)
+
+
+first = input("First: ")
+second = input("Second: ")
+sum = first + second
+print(sum)
