00b5ab2409e4c4607ff6296bb617e0d09dfac26a
khurram539 <108364751+khurram539@users.noreply.github.com>
2023-03-07 20:50:26 -0500

    Update

---
+++ Practice with Mosh/Prac8.py
@@ -0,0 +1,10 @@
+numbers = range (5)
+print (numbers)
+
+numbers = range (5, 10)
+for number in numbers:
+    print (number)
+
+numbers = range (5, 10, 2)
+for number in numbers:
+    print (number)
