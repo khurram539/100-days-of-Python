b8788c716428f3b215cdaa36950f0ab56c0f177c
khurram539 <108364751+khurram539@users.noreply.github.com>
2023-03-08 19:55:02 -0500

    update

--- 100 day of Python/Day 1.py
+++ 100 day of Python/Day 1.py
@@ -44,3 +44,9 @@ print("The function is declared like this:")
 print("This is my first python program")
 print("Hello" + " Khurram")
 
+#fix the code below
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
