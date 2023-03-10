b2478e8f4262e05d408a6da156e567d44a5f07ad
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-09 20:12:42 -0500

    Day 1 complete

--- 100 days of Python/Day 1.py
+++ 100 days of Python/Day 1.py
@@ -50,3 +50,39 @@ print("Day 1 - String Manipulation")
 print("String Concatenation is done with the "+" sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+# input () will get user input in console
+# Then print() will print the word "Hello" and the user input
+
+print("Hello " + input("what is your name?"))
+
+input ("What is your name? ")
+print(len("Khurram"))
+
+name = "khurram"
+print(name)
+
+name = "Angelina"
+print(name)
+
+# Dont change the code below
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
+
+# Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+# Ask the user for the city that they grew up in.
+city = print("What's name of the city you grew up in?\n")
+# ask the user for the name of a pet.
+print("What's your pet's name?\n")
+# Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " + pet)
+# Make sure the input cursor shows on a new line, see the example at:
+print("https://band-name-generator-end.appbrewery.repl.run/")
