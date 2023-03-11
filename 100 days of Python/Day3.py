ab93f204099d245312b19c086800d2f59c968596
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-10 23:59:11 -0500

    Day 3 Complete

--- 100 days of Python/Day3.py
+++ 100 days of Python/Day3.py
@@ -4,13 +4,182 @@ print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Please pay $5.")
+    elif age <= 18:
+        bill = 7
+        print("Please pay $7.")   
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!") 
+    else:
+        bill = 12
+        print("Please pay $12.")         
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += bill + 3
+    print(f"Your final bill is ${bill}.")
 else:
     print("Sorry, you have to grow taller before you can ride.")
  
+       
+
  
- # Even or Odd   
+  # Even or Odd   
 number = int(input("which number do want to check? "))
 if number % 2 == 0:
     print("This is an even number.")
 else:
     print("This is an odd number.")
+    
+# BMI Calculator 2.0
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+bmi = round(weight / height ** 2)
+if bmi < 18.5:
+    print(f"Your bmi is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your bmi is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your bmi is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your bmi is {bmi}, you are obese.")
+else:
+    print(f"Your bmi is {bmi}, you are clinically obese.")
+
+# Leap Year
+year = int(input("Which year do you want to check? "))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+        
+# pizza order
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+if size == "S":
+    bill = 15
+elif size == "M":
+    bill = 20
+elif size == "L":
+    bill = 25
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+if extra_cheese == "Y":
+    bill += 1
+print(f"Your final bill is: ${bill}.")
+
+# Love Calculator
+print ("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+combined_string = name1 + name2
+lower_case_string = combined_string.lower()
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+
+true = t + r + u + e
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+
+love = l + o + v + e
+
+love_score = int(str(true) + str(love))
+
+
+print(love_score)
+
+if love_score < 10 or love_score > 90:
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif love_score >= 40 and love_score <= 50:
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}.")
+
+# Treasure Island
+print( '''
+                     (
+                          )     (
+                   ___...(-------)-....___
+               .-""       )    (          ""-.
+         .-'``'|-._             )         _.-|
+        /  .--.|   `""---...........---""`   |
+       /  /    |                             |
+       |  |    |                             |
+        \  \   |                             |
+         `\ `\ |                             |
+           `\ `|                             |
+           _/ /\                             /
+          (__/  \                           /
+       _..---""` \                         /`""---.._
+    .-'           \                       /          '-.
+   :               `-.__             __.-'              :
+   :                  ) ""---...---"" (                 :
+    '._               `"--...___...--"`              _.'
+  jgs \""--..__                              __..--""/
+       '._     """----.....______.....----"""     _.'
+          `""--..,,_____            _____,,..--""`     
+*************************""----""*************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" ').lower()
+if choice1 == "left":
+    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. ').lower()
+    if choice2 == "wait":
+        choice3 = input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow, and one blue. Which color do you choose? ').lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
+if choice2 == "wait":
+    choice3 = input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow, and one blue. Which color do you choose? ').lower()
+    if choice3 == "red":
+        print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+        print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+        print("You enter a room of beasts. Game Over.")
+    else:
+        print("You chose a door that doesn't exist. Game Over.")
+else:
+    print("You get attacked by an angry trout. Game Over.")
+if choice3 == "red":
+    print("It's a room full of fire. Game Over.")
+if choice3 == "yellow":
+    print("You found the treasure! You Win!")
+if choice3 == "blue":
+    print("You enter a room of beasts. Game Over.")
+else:
+    print("You chose a door that doesn't exist. Game Over.")
+
+
+
+
+
+
+
+
