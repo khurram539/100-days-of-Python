b15ba0ec790925a3f31381fd055080d4607eaddc
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-10 22:29:03 -0500

    Day 3 In Progress

--- 100 days of Python/Day3.py
+++ 100 days of Python/Day3.py
@@ -6,3 +6,11 @@ if height >= 120:
     print("You can ride the rollercoaster!")
 else:
     print("Sorry, you have to grow taller before you can ride.")
+ 
+ 
+ # Even or Odd   
+number = int(input("which number do want to check? "))
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
