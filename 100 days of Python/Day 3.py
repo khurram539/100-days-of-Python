62363d78cc3d95760d5a2c50cdf196992b4372f7
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-11 00:01:19 -0500

    Day 3 Complete

--- 100 days of Python/Day 3.py
+++ 100 days of Python/Day 3.py
@@ -176,13 +176,6 @@ if choice3 == "blue":
 else:
     print("You chose a door that doesn't exist. Game Over.")
 
-def format_name(f_name, l_name):
-    """Take a first and last name and format it to return the title case version of the name."""
-    if f_name == "" or l_name == "":
-        return "You didn't provide valid inputs."
-    formatted_f_name = f_name.title()
-    formatted_l_name = l_name.title()
-    return f"{formatted_f_name} {formatted_l_name}"
 
 
 
