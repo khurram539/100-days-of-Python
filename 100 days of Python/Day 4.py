3b2f9fe80f27a5c46d155537b45a60ab66aac361
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-12 03:21:10 -0400

    Day 4 In Progress

--- 100 days of Python/Day 4.py
+++ 100 days of Python/Day 4.py
@@ -11,3 +11,18 @@ random_float * 5
 print(random_float)
 
 # Random module
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
+
+# List
+
+state1 = "California"
+state2 = "Oregon"
+states_of_america = ["California", "Oregon", "Washington", "Texas"]
+print(states_of_america[0])
+
+// you can change the value of the list by using the index number
