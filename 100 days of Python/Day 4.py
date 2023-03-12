66db19816ac9be743332becee9a2218cc9ef3170
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-11 23:20:58 -0500

    Day 4 In Progress

--- 100 days of Python/Day 4.py
+++ 100 days of Python/Day 4.py
@@ -1,1 +1,13 @@
 # Day 4 - Randomisation and Python Lists
+
+import random
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_float = random.random() 
+print(random_float)
+
+random_float * 5
+print(random_float)
+
+# Random module
