40367dd900a43b86bc5487793ae1b96e4f49034c
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-13 09:41:59 -0400

    Day 5 Complete

--- 100 days of Python/Day 5.py
+++ 100 days of Python/Day 5.py
@@ -65,6 +65,7 @@ for number in range(1, 101):
 
 import random
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+Cap_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 print ("Welcome to the PyPassword Generator!")
@@ -84,6 +85,9 @@ for car in range(1, nr_symbols + 1):
 
 for car in range(1, nr_numbers + 1):
     password_list += random.choice(number)
+for car in range(1, nr_numbers + 1):
+    password_list += random.choice(Cap_letters) 
+
   
 print(password_list)
 random.shuffle(password_list)
