2ed799dac48d91454ec78645dce081ae199bc9c7
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-25 14:52:43 -0400

    Day 8 In Progress

--- 100 days of Python/Day 8.py
+++ 100 days of Python/Day 8.py
@@ -20,4 +20,22 @@ def greet_with(name, location):
     print(f"Hello {name}")
     print(f"What is it like in {location}?")
     
-greet_with("Khurram", "Miami")
+greet_with("Nowhere", "Khurram")
+
+# Function arguments 
+
+greet_with(name="Khurram", location="Virginia")
+
+# Calculate the length of paint needed to paint the wall
+# 1 can of paint can cover 5 square meters of wall 
+# Given a random height and width of wall, calculate how many cans of paint you'll need to buy
+
+def paint_calc(height, width, cover):
+    area = height * width
+    cans = area / cover
+    print(f"You'll need {cans} cans of paint.")
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
