6d6356b6dd477d1a1d5aea14518c06835b7737e2
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-25 22:57:22 -0400

    Day 8 In Progress

--- 100 days of Python/Day 8.py
+++ 100 days of Python/Day 8.py
@@ -30,12 +30,15 @@ greet_with(name="Khurram", location="Virginia")
 # 1 can of paint can cover 5 square meters of wall 
 # Given a random height and width of wall, calculate how many cans of paint you'll need to buy
 
+import math 
 def paint_calc(height, width, cover):
     area = height * width
-    cans = area / cover
-    print(f"You'll need {cans} cans of paint.")
-
+    num_cans = math.ceil(area / cover)
+    print(f"You'll need {num_cans} cans of paint.")
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
+covrage = 5
+paint_calc(height=test_h, width=test_w, cover=covrage)
+   
+
+    
