7c9320074cb07b05337390487dbbffc1dd6d2993
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-26 00:30:45 -0400

    Day 8 Completed

--- 100 days of Python/Day 8.py
+++ 100 days of Python/Day 8.py
@@ -1,59 +1,55 @@
-# Day 8 - Function Parameters & Caesarer Cipher
+# Caeser cipher project
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def greet():
-    print("Hello")
-    print("How do you do?")
-    print("Isn't the weather nice today?")
-
-greet()
-
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
-
-greet_with_name("Khurram")
-
-# Positional Arguments VS Keyword Arguments
-
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}?")
-    
-greet_with("Nowhere", "Khurram")
-
-# Function arguments 
-
-greet_with(name="Khurram", location="Virginia")
-
-# Calculate the length of paint needed to paint the wall
-# 1 can of paint can cover 5 square meters of wall 
-# Given a random height and width of wall, calculate how many cans of paint you'll need to buy
-
-import math 
-def paint_calc(height, width, cover):
-    area = height * width
-    num_cans = math.ceil(area / cover)
-    print(f"You'll need {num_cans} cans of paint.")
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
-covrage = 5
-paint_calc(height=test_h, width=test_w, cover=covrage)
-
-# Prime Number Checker
-
-def prime_checker(number):
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-    if is_prime:
-        print("It's a prime number.")
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    #TODO-3: What happens if the user enters a number/symbol/space?
+    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
     else:
-        print("It's not a prime number.")
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+#TODO-1: Import and print the logo from art.py when the program starts.
+
+from art import logo
+print(logo)
+
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+  #Try running the program and entering a shift number of 45.
+  #Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+  #Hint: Think about how you can use the modulus (%).
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
+
+                  
 
 
 
 
 
-    
