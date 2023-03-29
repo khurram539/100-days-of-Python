85ff4797f4700c217032c3bf2203a0dc9eb54191
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-03-29 12:08:35 -0400

    Day 9 In Progress

--- 100 days of Python/Day 9.py
+++ 100 days of Python/Day 9.py
@@ -26,7 +26,78 @@ for thing in programming_dictionary:
     print(thing)
     print(programming_dictionary[thing])
     
+
+# Grading program
+
+student_scores = {
+    "Khurram": 81,
+    "Karim": 78,
+    "Amberina": 99,
+    "Angelina": 74,
+    "Aadam": 62,
+}    
     
+  
+
+student_grades = {}
+
+
+
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+        
+print(student_grades)
+
+
+# Nesting lists and dictionaries
+
+# Nesting 
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+    "Italy": "Rome",
+    "Spain": "Madrid",
+    "United Kingdom": "London",
+    "United States": "Washington, D.C.",
+    "Canada": "Ottawa",
+    "Mexico": "Mexico City",
+    "Brazil": "Brasilia",
+    "Argentina": "Buenos Aires",
+    "Colombia": "Bogota",
+    "Peru": "Lima",
+    "Venezuela": "Caracas",
+}
+
+# Nesting a list in a dictionary
     
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"],"total_visits": 12},
+    "Germany":  {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"],"total_visits": 5},
+    "Spain": {"cities_visited": ["Madrid", "Barcelona", "Seville"],"total_visits": 2},
+    "United Kingdom": {"cities_visited": ["London", "Birmingham", "Glasgow"],"total_visits": 3},
+    "Unite States": {"cities_visited": ["Washington, D.C.", "New York", "Miami"],"total_visits": 5},
+    "Canada": {"cities_visited": ["Ottawa", "Montreal", "Toronto"],"total_visits": 2},
+    "Mexico": {"cities_visited": ["Mexico City", "Guadalajara", "Cancun"],"total_visits": 3},
+    "Brazil": {"cities_visited": ["Brasilia", "Rio de Janeiro", "Sao Paulo"],"total_visits": 2},
+    "Argentina": {"cities_visited": ["Buenos Aires", "Cordoba", "Mendoza"],"total_visits": 1},
+    "Colombia": {"cities_visited": ["Bogota", "Medellin", "Cartagena"],"total_visits": 1},
+    "Peru": {"cities_visited": ["Lima", "Cusco", "Arequipa"],"total_visits": 1},
+    "Venezuela": {"cities_visited": ["Caracas", "Maracaibo", "Valencia"],"total_visits": 1},
+}
+  
     
-    
+               
+            
+   
+
+
+
