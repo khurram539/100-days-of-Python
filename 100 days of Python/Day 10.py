9450a87c715a0096ececfc3e207ac93b35ce48a2
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-04-20 23:14:41 -0400

    Day 10 Completed

--- 100 days of Python/Day 10.py
+++ 100 days of Python/Day 10.py
@@ -39,3 +39,136 @@ year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
+
+# Already used functions with outputs
+
+length = len(format_name)
+
+# Return as an early exit
+
+def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of the name."""
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    format_name = f_name.title()
+    format_name = l_name.title()
+    return f"{format_name} {format_name}"
+
+
+# quiz 1
+
+# Without running the code below, what do you think will be printed in the console?
+
+def add(n1, n2):
+  return n1 + n2
+ 
+def subtract(n1, n2):
+  return n1 - n2
+ 
+def multiply(n1, n2):
+  return n1 * n2
+ 
+def divide(n1, n2):
+  return n1 / n2
+ 
+print(add(2, multiply(5, divide(8, 4))))
+
+# quiz question 2
+
+# What would you predict to be the result of running the following code?
+
+def outer_function(a, b):
+    def inner_function(c, d):
+        return c + d
+    return inner_function(a, b)
+ 
+result = outer_function(5, 10)
+print(result)
+
+# quiz question 3
+
+# What will be printed in the console after running the following code?
+
+def my_function(a):
+    if a < 40:
+        return
+        print("Terrible")
+    if a < 80:
+        return "Pass"
+    else:
+        return "Great"
+print(my_function(25))
+
+# calculator project
+
+# add
+def add(n1, n2):
+    return n1 + n2
+
+# subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+# multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# divide
+def divide(n1, n2):
+    return n1 / n2
+
+operation = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+from replit import clear
+from art import logo
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      clear()
+      calculator()
+
+calculator()
+
+
+    
