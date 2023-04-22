316abe5e58b40c84e0e3d25ab3c741994686e1a9
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-04-22 16:28:22 -0400

    Day 11 Completed

--- 100 days of Python/Day 11 Project/Day 11.py
+++ 100 days of Python/Day 11 Project/Day 11.py
@@ -1,20 +1,23 @@
 # Day 11 - The Blackjack Capstone Project
 
+import random
+from replit import clear
+from art import logo
+
 def calculate_score(cards):
   """Take a list of cards and return the score calculated from the cards"""
 
-  #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+  
   if sum(cards) == 21 and len(cards) == 2:
     return 0
-  #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+  
   if 11 in cards and sum(cards) > 21:
     cards.remove(11)
     cards.append(1)
   return sum(cards)
 
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-def compare(user_score, computer_score):
-  #Bug fix. If you and the computer are both over, you lose.
+
+def compare(user_score, computer_score):  
   if user_score > 21 and computer_score > 21:
     return "You went over. You lose 😤"
 
@@ -35,7 +38,7 @@ def compare(user_score, computer_score):
     return "You lose 😤"
 
 def play_game():
-
+  
   print(logo)
 
   
