f6e084988fc2e4de5e0c95504636aa948a5d95c1
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-04-26 15:14:10 -0400

    Day 16 In Progress

---
+++ 100 days of Python/Day 16 Project/main.py
@@ -0,0 +1,21 @@
+# Day 16 - Oject Oriented Programming 
+
+
+
+import another_module
+
+print(another_module.another_variable)
+
+from turtle import turtle
+timmy = turtle()
+
+print(timmy)
+
+
+
+
+
+
+
+
+
