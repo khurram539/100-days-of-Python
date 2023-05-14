614f53db3bfa0bab5a662eadaa7898f6a74ba3f3
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-05-14 04:13:11 -0400

    Day 17 Completed

--- 100 days of Python/Day 17 Project/main.py
+++ 100 days of Python/Day 17 Project/main.py
@@ -1,27 +1,21 @@
-# Day 17 - Quiz  & the benefits of OOP
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
 
-class User:
-    def __init__(self, user_id, username):
-        self.id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
-        
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
-   
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-user_1 = User("001", "angela")
-user_2 = User("002", "jack")
+quiz = QuizBrain(question_bank)
 
-user_1.follow(user_2)
-print(user_1.followers)
-print(user_1.following)
-print(user_2.followers)
-print(user_2.following)
+while quiz.still_has_questions():
+    quiz.next_question()
 
-# 
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
 
 
 
