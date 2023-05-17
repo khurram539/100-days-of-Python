59886bd30d4a664d3c159c4ed5705699eaa386df
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-05-17 11:07:39 -0400

    Day 21 In Progress

---
+++ 100 days of Python/Day 21 Project/main.py
@@ -0,0 +1,23 @@
+# Day 21 - Build the Snake Game Part 2: inheritence and list slicing
+
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale.")
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()        
+   
+
+    def breathe(self):
+        super().breathe()
+        print("doing this underwater.")
+
+    def swim(self):
+        print("moving in water.")
+
+nemo = Fish()
+nemo.breathe()
