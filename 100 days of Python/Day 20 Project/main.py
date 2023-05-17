120e53edbdba8e74d1b8881c86b46d736ec69ee5
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-05-17 09:45:10 -0400

    Day 20 Completed

--- 100 days of Python/Day 20 Project/main.py
+++ 100 days of Python/Day 20 Project/main.py
@@ -1,43 +1,39 @@
 # Day 20 - Build the Snake Game Part 1: Animation & Coordinates
 
 
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0) # Turns off animation
+screen.title("My Snake Game")
+screen.tracer(0)
 
-segments = []
+snake = Snake()
+food = Food()
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-    
-    
-
-    
-    
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        # print(seg_num)
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        # print(new_x, new_y)
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
+    snake.move()
+
+
+screen.exitonclick() 
+
+
+
+    
+
         
         
     
