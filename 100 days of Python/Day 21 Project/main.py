f5d8870bd83f5fada25f56e5d55bd15b7ee56f6d
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-05-31 00:52:20 -0400

    Day 21 Complete

--- 100 days of Python/Day 21 Project/main.py
+++ 100 days of Python/Day 21 Project/main.py
@@ -1,23 +1,54 @@
 # Day 21 - Build the Snake Game Part 2: inheritence and list slicing
 
-class Animal:
-    def __init__(self):
-        self.num_eyes = 2
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
 
-    def breathe(self):
-        print("Inhale, exhale.")
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
-class Fish(Animal):
-    def __init__(self):
-        super().__init__()        
-   
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-    def breathe(self):
-        super().breathe()
-        print("doing this underwater.")
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-    def swim(self):
-        print("moving in water.")
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
-nemo = Fish()
-nemo.breathe()
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+
+
+
+screen.exitonclick()
