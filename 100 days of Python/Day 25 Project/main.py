3ced77bb243a3b1172d3648f2629e236469798b9
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-08-17 16:02:32 -0400

    Day 25 In Progress

--- 100 days of Python/Day 25 Project/main.py
+++ 100 days of Python/Day 25 Project/main.py
@@ -1,7 +1,22 @@
 # Day 24 - Working with CSV files
 
-with open("weather_data.csv") as data_file:
-    data = data_file.readlines()
-    print(data)
 
-    
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# # print(data["temp"])
+
+data_dict = data.to_dict()   
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+average = sum(temp_list) / len(temp_list)
+print(average)
+
+print(data["temp"].mean())
+data["temp"] = data["temp"] * 9/5 + 32
+print(data["temp"].max())
+print(data[data.temp == data.temp.max()])
