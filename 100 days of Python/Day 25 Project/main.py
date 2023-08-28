0479627364fc5991e8e146637df3650f4604018a
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-08-28 15:55:38 -0400

    Day 25 In Progress

--- 100 days of Python/Day 25 Project/main.py
+++ 100 days of Python/Day 25 Project/main.py
@@ -1,26 +1,58 @@
 # Day 24 - Working with CSV files
 
 
-with open ("weather_data.csv") as data_file:
-    data = data_file.readlines()
-    print(data)
-
-
+import pandas
 import csv
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row [1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+# data = pandas.read_csv("weather_data.csv")
+# # # print(type(data))
+# # print(type(data["temp"]))
+
+# data_dict = data.to_dict()
+# print(data_dict)
 
 
-import pandas
+# temp_list = data["temp"].to_list()
+# print(len(temp_list))
 
-data = pandas.read_cvs("weather_data.cvs")
-print(data["temp"])
+# # average = sum(temp_list) / len(temp_list)
+# # print(average)
+
+
+
+# # print(data["temp"].mean())
+# # print(data["temp"].max())
+
+# # # Get data in columns
+
+# # print(data["condition"])
+# # print(data.condition)
+
+
+# # Get data in rows 
+
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp[0]
+# monday_temp_F = monday_temp * 9/5 + 32
+# print(monday_temp_F)
+
+
+# create a dataframe from scratch
+
+data_dict = {
+    "students": ["Alex", "James", "Geni"],
+    "scores": [80, 79, 93]
+
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
+
+
+
 
 
 
