05fdc61ab78ffa67380d64ef52c12ab574ff7b9c
Khurram Khoja <108364751+khurram539@users.noreply.github.com>
2023-08-29 15:46:26 +0000

    Day 25 Project Complete

--- 100 days of Python/Day 25 Project/main.py
+++ 100 days of Python/Day 25 Project/main.py
@@ -42,16 +42,30 @@ import csv
 
 # create a dataframe from scratch
 
-data_dict = {
-    "students": ["Alex", "James", "Geni"],
-    "scores": [80, 79, 93]
+# data_dict = {
+#     "students": ["Alex", "James", "Geni"],
+#     "scores": [80, 79, 93]
 
+# }
+
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
 }
 
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
-
-
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
 
 
 
