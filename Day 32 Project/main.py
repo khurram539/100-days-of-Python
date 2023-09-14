a8d734aeb98ed04baeea89179535b9192cbb1bc7
Your Name <khurram539@hotmail.com>
2023-09-14 19:18:02 +0000

    Day 32 In Progress

--- Day 32 Project/main.py
+++ Day 32 Project/main.py
@@ -1,1 +1,18 @@
 # Day 32 - Send emails and manages dates
+
+
+import smtplib
+
+my_email = "xxxxxxxxxxx"
+password = "xxxxxxxxxxx"
+
+
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password="XXXXXXXX")
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="xxxxxxxxxxx",
+        msg="Subject:Hello\n\nThis is the body of my email."
+    )
