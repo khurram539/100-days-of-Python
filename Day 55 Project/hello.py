09c6eef6bb5290d4a9c64468010042976ebd711b
Your Name <khurram539@hotmail.com>
2024-04-02 12:18:10 -0400

    Day 55 In Progress

--- Day 55 Project/hello.py
+++ Day 55 Project/hello.py
@@ -4,25 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '<h1 style="text-align: center">Hello World!<h1>' \
+           '<p>This is a smooch time!</p>' \
+           '<iframe src="https://giphy.com/embed/9G0AdBbVrkV3O" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 
 @app.route('/bye')
 def bye():
-    return 'Bye!'
+    return "Bye!"
 
-
-@app.route("/<name>")
-def greet(name):
-    return f"Hello {name}!"
+# Creating variable path and converting the path to a specific data type
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello there {name}, you are {number} years old!"
 
 if __name__ == "__main__":
+    #Run the app in debug mode to auto-reload
     app.run(debug=True)
-
-
-# export FLASK_APP=hello.py
-# flask run
-
-
-
+    
+    
     
     
