4772026f4d8cfbab30f612b4ec3776727fc3fcec
Your Name <khurram539@hotmail.com>
2024-04-16 15:50:56 -0400

    Day 56 In Progress

--- Day 56 Project/server.py
+++ Day 56 Project/server.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return render_template("Angela.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# export FLASK_APP=server.py
+# flask run# 
