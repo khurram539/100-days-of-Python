2acc97fa1bba2e4eb614b0180aa860894c8aec30
Your Name <khurram539@hotmail.com>
2024-04-17 16:55:16 -0400

    Finished Demo

--- Day 56 Project/server.py
+++ Day 56 Project/server.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("Angela.html")
+    return render_template("Khurram.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 
 # export FLASK_APP=server.py
-# flask run# 
+# flask run 
