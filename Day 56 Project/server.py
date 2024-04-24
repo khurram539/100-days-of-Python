9b65efe56e2d6403f402bbc2c8e4eead71439406
Your Name <khurram539@hotmail.com>
2024-04-24 17:16:25 -0400

    Day 56 In Progress

--- Day 56 Project/server.py
+++ Day 56 Project/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("Khurram.html")
+    return render_template("Angela.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
