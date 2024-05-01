2ac618debe3267bc6731cc23771be0aa6eabafb9
Your Name <khurram539@hotmail.com>
2024-05-01 03:14:47 -0400

    Day 57 In Progress

---
+++ Day 57 Project/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    random_number = random.randint(1, 10)
+    return render_template("index.html", num=random_number)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# export FLASK_APP=server.py
+# flask run 
+# or python server.py
