12d11855c70cbdec972ec5d071c3e2726f76c373
Your Name <khurram539@hotmail.com>
2024-05-01 17:05:33 -0400

    Multiline Statement w/ Jinja

--- Day 57 Project/server.py
+++ Day 57 Project/server.py
@@ -3,8 +3,6 @@ import random
 import datetime
 import requests
 
-
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,8 +10,6 @@ def home():
     random_number = random.randint(1, 10)
     current_year = datetime.datetime.now().year
     return render_template("index.html", num=random_number, year=current_year)
-
-
 
 @app.route("/guess/<name>")
 def guess(name):  
@@ -27,7 +23,12 @@ def guess(name):
     age = age_data["age"]
     return render_template("guess.html", person_name=name, gender=gender, age=age)
 
-
+@app.route("/blog/<num>")
+def blog():
+    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
