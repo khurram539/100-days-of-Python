162da9630209b5a781728dfbe91b4bda3af1c8be
Your Name <khurram539@hotmail.com>
2024-05-01 09:39:49 -0400

    Day 57 In Progress

--- Day 57 Project/server.py
+++ Day 57 Project/server.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
 import random
+import datetime
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     random_number = random.randint(1, 10)
-    return render_template("index.html", num=random_number)
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", num=random_number, year=current_year)
+
+
+
+@app.route("/guess/<name>")
+def guess(name):  
+    current_age = datetime.datetime.now().year - 1995
+    return render_template("guess.html", person_name=name, age=current_age)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
