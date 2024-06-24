7f5b79a0ec8a423f41b5fd90996f1686b6194a22
Your Name <khurram539@hotmail.com>
2024-06-24 17:36:25 -0400

    Password field

--- Day 61 Project/main.py
+++ Day 61 Project/main.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField,PasswordField,SubmitField
+
+app = Flask(__name__)
+app.secret_key = "BlankMisticalBEAST@1324"
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = StringField('Password')
-
-
-app = Flask(__name__)
-app.secret_key = "any-string-you-want-just-keep-it-secret"
+    email = StringField(label='email')
+    password = PasswordField(label='password')
+    submit = SubmitField(label='Login')
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    login_form = LoginForm()
-    return render_template('login.html', form=login_form)
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+# export FLASK_APP=main.py
+# flask run
